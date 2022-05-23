1e889407f8d09900e5beacbd4e35587c2aa3cffc
Katy Andersen <katherineandersen@sandiego.edu>
2022-05-23 12:09:32 -0400

    Removed destructuring off sign in with username and password function -- no longer needed due to previous centralization

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -29,10 +29,7 @@ const SignInForm = () => {
     event.preventDefault();
 
     try {
-      const { user } = await signInAuthUserWithEmailAndPassword(
-        email,
-        password
-      );
+      await signInAuthUserWithEmailAndPassword(email, password);
       resetFormFields();
     } catch (error) {
       switch (error.code) {
