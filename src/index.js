bde8df1bc3d33c0928fbfad4f1a7792201a7b9ac
Katy Andersen <katherineandersen@sandiego.edu>
2022-05-23 15:55:49 -0400

    Added shop route and product card component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-
 import App from "./App";
 import { UserProvider } from "./contexts/user.context";
+import { ProductsProvider } from "./contexts/products.context";
 import reportWebVitals from "./reportWebVitals";
 import "./index.scss";
 
@@ -12,7 +12,9 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
-        <App />
+        <ProductsProvider>
+          <App />
+        </ProductsProvider>
       </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
