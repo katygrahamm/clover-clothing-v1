7601c9211aaef196959ca87c88e4c7d2780fdd60
Katy Andersen <katherineandersen@sandiego.edu>
2022-05-26 20:15:21 -0400

    Built firebase utility functions for adding collections and mapping the data

--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -1,13 +1,23 @@
-import { createContext, useState } from "react";
-import PRODUCTS from "../shop-data.json";
+import { createContext, useState, useEffect } from "react";
+import { getCategoriesAndDocuments } from "../utils/firebase/firebase.utils.js";
+import SHOP_DATA from "../shop-data.js";
 
 export const ProductsContext = createContext({
-  products: [PRODUCTS],
+  products: [],
   setProducts: () => [],
 });
 
 export const ProductsProvider = ({ children }) => {
-  const [products, setProducts] = useState(PRODUCTS);
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const getCategoriesMap = async () => {
+      const categoryMap = await getCategoriesAndDocuments();
+      console.log(categoryMap);
+    };
+    getCategoriesMap();
+  }, []);
+
   const value = { products };
 
   return (
